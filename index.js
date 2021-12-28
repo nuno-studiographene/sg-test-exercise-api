1011d1578222f206a024b72c01f218819588e5c7
Nuno <nuno@studiographene.com>
2021-12-28 11:49:48 +0000

    trying to deploy 8

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import foodsRoutes from './routes/foods.js';
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
